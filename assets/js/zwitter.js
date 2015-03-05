0be42ce7e479498ffd4f562ae4b274286f53be83
Darragh <darragh@rocketmedia.ie>
2015-03-05 23:03:33 +0000

    wip

--- assets/js/zwitter.js
+++ assets/js/zwitter.js
@@ -2,22 +2,29 @@
 
   'use strict';
 
-  var alert = document.getElementById('alert'),
-      group = document.getElementById('group'),
-      name  = document.getElementById('name'),
-      regex = /^\w{1,15}$/;
+  var elem = document.createElement('div'),
+      form = document.forms.twitter,
+      list = form.classList,
+      re   = /^\w{1,15}$/;
 
-  name.addEventListener('input', function() {
-    if (!regex.test(this.value)) {
-      group.classList.add('has-error');
+  form.addEventListener('input', function() {
+    if (list.contains('form-init')) {
+      list.remove('form-init');
+    }
+    if (re.test(this.username.value)) {
+      list.remove('form-error');
     } else {
-      group.classList.remove('has-error');
+      list.add('form-error');
     }
   }, false);
 
-  // elem.innerHTML = ['@', '&#8203', name.value].join('');
-  // window.prompt('Press Ctrl + C and Enter', elem.textContent);
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (re.test(this.username.value)) {
+        elem.innerHTML = ['@', '&#8203', this.username.value].join('');
+        var r = window.prompt('Press Ctrl + C and Enter', elem.textContent);
+        alert(r);
+    }
+  }, false);
 
 })();
-
-
